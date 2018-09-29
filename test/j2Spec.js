dd14650b7da524f41d2639ab263eb16d5b4646bc
lroche <1540492+lroche@users.noreply.github.com>
2018-09-29 14:55:17 +0200

    Asynchronous Spec wih done function didn't work, closed #9

--- test/j2Spec.js
+++ test/j2Spec.js
@@ -5,12 +5,39 @@ define([
     'use strict';
     //Goal : check if any feature of Jasmine 2 is not broken  
     describe("In jasmine2 environnement", function(){
-        it("it supports async callback with done function", function(done){
-            expect(done).toBeDefined();
-        });
-        it("fail() method should be support", function(done){
-            expect(done.fail).toBeDefined();           
+        describe("done method", function(){
+            var ok;
+            it("should be supported to declare an asynchronous spec", function(done){
+                expect(done).toBeDefined();
+                setTimeout(function(){
+                    ok = true;
+                    done();
+                },100);
+                ok = false;
+            });
+            it("fail() method should be supported", function(done){
+                
+                expect(done.fail).toBeDefined();           
+                if(!ok){
+                    done.fail("done should create correcty an asynchronous spec");
+                }
+                done();
+            })
             
-        })
+        });
+        describe("beforeEach/afterEach", function(){
+            var isInit;
+            beforeEach(function(done){
+                expect(done).toBeDefined();
+                setTimeout(function(){
+                    isInit = true;
+                    done();
+                }, 100);
+                isInit = false;
+            });
+            it("should support correctly done() method", function(){
+                expect(isInit).toBe(true);
+            });
+        });
     })  
 });
